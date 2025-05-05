fa4f92cc47652ada48f0a909813333a62a18702f
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2025-05-05 17:38:20 +0200

    PowerShell ohne StowDir Push

--- ps/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ ps/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,11 @@
 # UPDATE STOW REPO (nur Montags)
-if ([DateTime]::Now.DayOfWeek -eq [System.DayOfWeek]::Monday) {
-    cd ~/stowdir
-    git add .
-    git commit -m "Aktuelle Version"
-    git push
-    cd ~
-}
+#if ([DateTime]::Now.DayOfWeek -eq [System.DayOfWeek]::Monday) {
+#    cd ~/stowdir
+#    git add .
+#    git commit -m "Aktuelle Version"
+#    git push
+#    cd ~
+#}
 
 function stopgui {
     [CmdletBinding()]
